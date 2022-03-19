6a132014389a052e8fdca6048380a049a492e78a
ariel <arielugm@gmail.com>
2022-03-19 01:43:45 -0600

    test JSI.js  Resuelto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
  //En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "camaras";
+const nuevaString = "cctv";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 20;
@@ -271,7 +271,7 @@ function esVocal(letra){
   
   if (letra.length > 1) {
     return "Dato incorrecto";
-    
+
   }
   if (letra === "a") {
     return "Es vocal";
